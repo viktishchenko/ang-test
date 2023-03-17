cf61142dadc3d90e30b0976ef48eca1cdd194fe9
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-03-17 12:24:18 +0600

    fin: get input data, save, filtered

--- base-todo/src/app/components/todo-base/todo-base.component.ts
+++ base-todo/src/app/components/todo-base/todo-base.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export interface IDataList {
+  id: number;
+  task: string;
+}
+
 @Component({
   selector: 'app-todo-base',
   templateUrl: './todo-base.component.html',
-  styleUrls: ['./todo-base.component.scss']
+  styleUrls: ['./todo-base.component.scss'],
 })
 export class TodoBaseComponent {
+  list: IDataList[] = [];
 
+  addTask(item: string) {
+    this.list.push({ id: this.list.length, task: item });
+    console.log(this.list);
+  }
+
+  removeTask(id: number) {
+    console.log('id', id);
+    this.list = this.list.filter((item) => item.id !== id);
+    console.log('this.list', this.list);
+  }
 }
