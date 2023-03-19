1b07d0677ac1d6618a84360cbdd9d551bff049da
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-03-19 08:10:34 +0600

    feat: reactive form basic validation

--- base-todo/src/app/components/reactive-form/reactive-form.component.ts
+++ base-todo/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,12 +7,28 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent {
+  emailPattern =
+    '([A-Za-z0-9][._]?)+[A-Za-z0-9]@[A-Za-z0-9]+(.?[A-Za-z0-9]){2}.(com?|net|org|ru)+(.[A-Za-z0-9]{2,4})?';
+
   loginForm = new FormGroup({
-    user: new FormControl(''),
-    password: new FormControl(''),
+    user: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern(this.emailPattern),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
   loginUser() {
     console.log('this.loginForm>>', this.loginForm.value);
+  }
+  get user() {
+    return this.loginForm.get('user');
+  }
+  get password() {
+    return this.loginForm.get('password');
   }
 }
