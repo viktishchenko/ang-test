92fcb4e16356a65ec8d63c8f203f72a1f5003b58
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-03-20 11:31:05 +0600

    feat: add event binding

--- base-routing/src/app/home/home.component.ts
+++ base-routing/src/app/home/home.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  flag = true;
+  greeting = 'Welcome!';
+  greet = '';
+  bye = 'Bye!';
+  handleClick() {
+    this.flag = !this.flag;
+  }
+  eventClick(event: any) {
+    console.log('event', event);
+  }
 }
