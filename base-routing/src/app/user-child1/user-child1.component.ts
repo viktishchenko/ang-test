6b02dd33ab3694d6f31c743c98dfc62fdff7240a
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-03-20 10:32:01 +0600

    feat: css conditional rendering, style binding

--- base-routing/src/app/user-child1/user-child1.component.ts
+++ base-routing/src/app/user-child1/user-child1.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-child1',
   templateUrl: './user-child1.component.html',
-  styleUrls: ['./user-child1.component.scss']
+  styleUrls: ['./user-child1.component.scss'],
 })
 export class UserChild1Component {
-
+  conditionalRendering = true;
+  styleBinding = 'orange';
+  objectStyleBinding = {
+    color: 'blue',
+    fontStyle: 'italic',
+  };
 }
