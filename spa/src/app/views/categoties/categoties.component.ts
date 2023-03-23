59dee55713c4940754e703ca959ee97194909c94
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-03-23 21:44:34 +0600

    feat: add bootstrap/Amaterial, app structure"

---
+++ spa/src/app/views/categoties/categoties.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoties',
+  templateUrl: './categoties.component.html',
+  styleUrls: ['./categoties.component.scss']
+})
+export class CategotiesComponent {
+
+}
