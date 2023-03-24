b1102b7ec4896c39d698aa6a326eaf4f9ab89797
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-03-24 13:30:03 +0600

    feat: data to component w service, display menu category

--- spa/src/app/services/data.service.ts
+++ spa/src/app/services/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ICategory } from '../models/category';
+import { categories } from '../data/testData';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor() {}
 
-  constructor() { }
+  getCategories(): ICategory[] {
+    return categories;
+  }
 }
--- spa/src/app/views/categoties/categoties.component.ts
+++ spa/src/app/views/categoties/categoties.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICategory } from 'src/app/models/category';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-categoties',
   templateUrl: './categoties.component.html',
-  styleUrls: ['./categoties.component.scss']
+  styleUrls: ['./categoties.component.scss'],
 })
-export class CategotiesComponent {
+export class CategotiesComponent implements OnInit {
+  categories: ICategory[] = [];
+  constructor(private dataHandler: DataService) {}
 
+  ngOnInit() {
+    this.categories = this.dataHandler.getCategories();
+    console.log('this.categories', this.categories);
+  }
 }
