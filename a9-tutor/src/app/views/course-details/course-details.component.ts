60bb7aeee0a996584d3113c4f2a35566050a7669
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-04-04 17:17:24 +0600

    feat: pass data/events to/from component

--- a9-tutor/src/app/views/course-details/course-details.component.ts
+++ a9-tutor/src/app/views/course-details/course-details.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ISelectedCourse } from 'src/app/models/courses';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
-  styleUrls: ['./course-details.component.scss']
+  styleUrls: ['./course-details.component.scss'],
 })
 export class CourseDetailsComponent {
-
+  @Input()
+  selected!: ISelectedCourse;
+  @Output() save = new EventEmitter();
+  @Output() clear = new EventEmitter();
 }
