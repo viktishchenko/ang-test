f229b1bb011bf4ef6c2058a3dfc7f90270d76e0a
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-04-12 12:47:40 +0600

    feat: add nav comp w data/model

---
+++ front/src/app/data/testData.ts
@@ -0,0 +1,15 @@
+import { IMenu } from '../models/menu';
+
+export const menu: IMenu[] = [
+  {
+    id: 1,
+    title: 'Главная',
+    href: '/',
+  },
+  {
+    id: 2,
+    title: 'Пользователи',
+    href: '/users',
+    icon: 'be be-people',
+  },
+];
